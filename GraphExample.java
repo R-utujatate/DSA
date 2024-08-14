c93916bf9f78f3776bd71adf631200e7a9ab5d72
R-utujatate <156331583+R-utujatate@users.noreply.github.com>
2024-08-15 00:08:42 +0530

    Update GraphExample.java

--- GraphExample.java
+++ GraphExample.java
@@ -57,7 +57,49 @@ public class GraphExample {
         System.out.println();
 
     }
-    //Funct
+    //Function to perform Depth-First Search traversal of the graph
+    public void dfs(int startVertex)
+    {
+        boolean[] visited = new boolean[V];
+        dfsUtil(startVertex,visited);
+        System.out.println();
+    }
+
+    private void dfsUtil(int vertex,boolean[] visited)
+    {
+        visited[vertex]=true;
+        System.out.println(vertex+" ");
+
+        List<Integer>neighbors = adjacencyList.get(vertex);
+        for (int neighbor:neighbors)
+        {
+            if (!visited[neighbor])
+            {
+                dfsUtil(neighbor,visited);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+            //Number of vertices
+        int V=5;
+        GraphExample graph = new GraphExample(V);
+
+        //Add edges
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(1,3);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+
+        System.out.println("BFS traversal: ");
+        graph.bfs(0);
+
+        System.out.println("DFS traversal: ");
+        graph.dfs(0);
+
+    }
 
 
 }
